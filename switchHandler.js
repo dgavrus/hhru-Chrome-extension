5bafe303d95e3bf3dc97cc670a94cd6560b0fe28
dgavrus <denis.gavrus@gmail.com>
2014-08-22 12:02:40 +0400

    manipulations with switch. handler works.

--- switchHandler.js
+++ switchHandler.js
@@ -1,8 +1,17 @@
-window.addEventListener("load", function()
-{
+window.addEventListener("load", function(){
     document.getElementById("myonoffswitch").addEventListener("click", onClickSwitch, false);
 }, false);
 
-onClickSwitch = function(){
-    alert("click!");
+
+
+onClickSwitch = function(tab){
+    var checked = !!!($("#myonoffswitch").attr("checked"));
+    $("#myonoffswitch").attr("checked", checked);
+    if(checked){
+        chrome.tabs.executeScript(tab.id, {file:"alertText.js"});
+    }
+    else{
+        chrome.tabs.executeScript(tab.id, {code:"alert('111')"});
+    }
+    console.log("after:" + checked);
 }
